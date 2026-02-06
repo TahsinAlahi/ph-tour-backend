38b8902c8adf211091f329c09a7f95a2c7a4260e
Tahsin Alahi <alahitahsin@gmail.com>
2026-02-06 13:14:52 +0600

    feat: add error handling for server events

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect("");
+    await mongoose.connect("mongodb://localhost:27017/ph-tour");
     server = app.listen(5000, () => {
       console.log("The server is running on port 5000");
     });
@@ -17,3 +17,47 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on("unhandledRejection", (err) => {
+  console.log(
+    "Unhandled rejection is detected, shutting down the server.\n" + err,
+  );
+  if (server) {
+    server.close(() => {
+      console.log(err);
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(
+    "Uncaught exception is detected, shutting down the server.\n" + err,
+  );
+  if (server) {
+    server.close(() => {
+      console.log(err);
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM is received");
+  if (server) {
+    server.close();
+    process.exit(0);
+  }
+  process.exit(0);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT is received, shutting down the server.");
+  if (server) {
+    server.close();
+    process.exit(0);
+  }
+  process.exit(0);
+});
