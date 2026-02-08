949a28159084e2dc1ad80d7f38b738c5fdac9266
Tahsin Alahi <alahitahsin@gmail.com>
2026-02-08 10:27:53 +0600

    feat: setup env config

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
-import GracefulShutDown from "./utils/GracefulShutDown.util";
+import GracefulShutDown from "./app/utils/GracefulShutDown.util";
+import { envConfig } from "./app/config/env";
 
 export let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ph-tour");
-    server = app.listen(5000, () => {
-      console.log("The server is running on port 5000");
+    await mongoose.connect(envConfig.DB_URL);
+    server = app.listen(envConfig.PORT, () => {
+      console.log(`The server is running on port ${envConfig.PORT}`);
     });
     // setup graceful shutdown
     new GracefulShutDown(server);
