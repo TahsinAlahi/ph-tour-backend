dc86dc9c03a02ebe485ab8acad37dc3e140290e2
Tahsin Alahi <alahitahsin@gmail.com>
2026-02-22 03:19:42 +0600

    feat: create request body validation

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../../middleware/validateRequest";
 
 const router = Router();
 
-router.post("/register", userController.createUser);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  userController.createUser,
+);
 
 export const userRoutes = router;
