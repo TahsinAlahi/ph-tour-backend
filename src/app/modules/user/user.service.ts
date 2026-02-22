985df51ab99527769a732c1670340a90b0855b9a
Tahsin Alahi <alahitahsin@gmail.com>
2026-02-23 03:55:01 +0600

    feat: hashing password

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,22 @@
-import { IUser } from "./user.interface";
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email } = payload;
-  const user = await User.create({ name, email });
+  const { email, ...rest } = payload;
+  const doesUserExist = await User.exists({ email });
+  if (doesUserExist) {
+    throw new AppError(StatusCodes.CONFLICT, "The email already exists!!");
+  }
+
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email!,
+  };
+
+  const user = new User({ email, auths: [authProvider], ...rest });
+  await user.save();
 
   return user;
 };
