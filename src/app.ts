088680d660859487026a6c8b9ef44b4b4c5593ca
Tahsin Alahi <alahitahsin@gmail.com>
2026-03-15 17:18:14 +0600

    feat: working on passport google auth

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,16 @@ import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import routeNotFound from "./app/middleware/routeNotFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 
 const app = express();
 
+app.use(
+  expressSession({ secret: "", resave: false, saveUninitialized: false }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -12,6 +12,11 @@ interface EnvConfigVar {
   BCRYPT_SALT_ROUND: string;
   SUPER_ADMIN_EMAIL: string;
   SUPER_ADMIN_PASSWORD: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  GOOGLE_CALLBACK_URL: string;
+  EXPRESS_SESSION_SECRET: string;
+  FRONTEND_URL: string;
 }
 
 function loadEnvConfig() {
@@ -26,6 +31,11 @@ function loadEnvConfig() {
     "BCRYPT_SALT_ROUND",
     "SUPER_ADMIN_EMAIL",
     "SUPER_ADMIN_PASSWORD",
+    "GOOGLE_CLIENT_ID",
+    "GOOGLE_CLIENT_SECRET",
+    "GOOGLE_CALLBACK_URL",
+    "EXPRESS_SESSION_SECRET",
+    "FRONTEND_URL",
   ] as const;
 
   requiredVariables.forEach((key) => {
@@ -45,6 +55,11 @@ function loadEnvConfig() {
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND,
     SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL,
     SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+    EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET,
+    FRONTEND_URL: process.env.FRONTEND_URL,
   } as EnvConfigVar;
 }
 
