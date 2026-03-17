44d612f6e3ef2c2c1ee5dd7e631a6fe405cf3d69
Tahsin Alahi <alahitahsin@gmail.com>
2026-03-17 15:15:34 +0600

    feat: finished passport config

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,6 +1,11 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import {
+  Strategy as GoogleStrategy,
+  Profile,
+  VerifyCallback,
+} from "passport-google-oauth20";
 import { envVars } from "./env";
+import { User } from "../modules/user/user.model";
 
 passport.use(
   new GoogleStrategy(
@@ -9,6 +14,32 @@ passport.use(
       clientSecret: envVars.GOOGLE_CLIENT_SECRET,
       callbackURL: envVars.GOOGLE_CALLBACK_URL,
     },
-    async () => {},
+    async (
+      _accessToken: string,
+      _refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback,
+    ) => {
+      try {
+        const email = profile.emails![0].value;
+        if (!email) {
+          return done(null, false, { message: "Email not found" });
+        }
+        let user = await User.findOne({ email });
+        if (!user) {
+          user = await User.create({
+            name: profile.displayName,
+            email: profile.emails![0].value,
+            picture: profile.photos![0].value,
+            auths: [{ provider: "google", providerId: profile.id }],
+            isVerified: true,
+          });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    },
   ),
 );
